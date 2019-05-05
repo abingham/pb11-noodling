33fe5b2ccac0d5cb15f319680b4ff9e90e974389
Austin Bingham <austin.bingham@gmail.com>
2019-05-05 10:27:05 +0200

    Demo of iteration.

--- test_zivid.py
+++ test_zivid.py
@@ -1,7 +1,12 @@
+from itertools import islice
 import zivid
 
 app = zivid.Application()
 camera = zivid.Camera(app)
+
+# iteration
+for datum in islice(app, 10):
+    print(datum)
 
 # We delete the app to give Python a chance to garbage collect it. When that
 # happens, setting camera data should cause a segfault...unless we establish the
